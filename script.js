f294312f3f89bb86dd45b74802ed9fb084ae5c29
0bamaman <132011740+colesheyka@users.noreply.github.com>
2023-05-24 15:22:35 -0700

    fixed margins

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-
-
-// Get the button element
-var backToTopButton = document.getElementById("back-to-top");
 document.addEventListener("DOMContentLoaded", function() {
     var backToTopButton = document.getElementById("back-to-top");
   
-    // Show or hide the button based on the user's scroll position
-    window.addEventListener("scroll", function() {
-      console.log("JavaScript code executed!");
-  
-      if (window.pageYOffset > 5950) {
-        // Show the button when the user has scrolled down 300 pixels
-        backToTopButton.style.display = "block";
-      } else {
-        // Hide the button when the user is near the top of the page
-        backToTopButton.style.display = "none";
-      }
-    });
-  
+// Show or hide the button based on the user's scroll position
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 6050) {
+      // Show the button when the user has scrolled down 300 pixels
+      backToTopButton.style.display = "block";
+    } else {
+      // Hide the button when the user is near the top of the page
+      backToTopButton.style.display = "none";
+    }
+  });
+
+    // Scroll to top when the button is clicked
     backToTopButton.addEventListener("click", function() {
       console.log("Button clicked!");
       window.scrollTo({
@@ -25,11 +20,10 @@ document.addEventListener("DOMContentLoaded", function() {
         behavior: "smooth"
       });
     });
-  });
   
-  document.addEventListener("DOMContentLoaded", function() {
     var videoElement = document.getElementById("my-video");
   
     // Mute the video
     videoElement.muted = true;
   });
+  
